bde79ac0d27f050a51fd5b9b362650b65ed0d4dd
Marija Topalova <marijatopalova1@outlook.com>
2024-11-22 00:24:41 +0100

    added CreateProductCommand

---
+++ SCMS.Application/Dtos/OrderDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCMS.Application.Dtos
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItemDto> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+
+    public class OrderItemDto
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ SCMS.Application/Features/Products/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SCMS.Application.Interfaces.Repositories;
+using SCMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCMS.Application.Features.Products.Commands
+{
+    public class CreateProductCommandHandler(IProductRepository productRepository) 
+        : IRequestHandler<CreateProductCommand, Guid>
+    {
+        public async Task<Guid> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product(command.Name, command.Description, command.Price, command.QuantityInStock);
+
+            await productRepository.AddAsync(product);
+
+            return product.Id;
+        }
+    }
+}
